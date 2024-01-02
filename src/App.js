2f800f453035052d50edd4ee76d7f575739a5464
ahmetfatihay <77299600+ahmetfatihay@users.noreply.github.com>
2024-01-03 00:43:10 +0300

    fix commit

--- src/App.js
+++ src/App.js
@@ -3,43 +3,23 @@ import { useState } from "react";
 const data = [
   {
     employeeId: "",
-    name: "ODTU",
+    name: "",
     sayi1: "0",
     sayi2: "0",
     sayi3: "0",
     sayi4: "0",
     sayi5: "0",
-    overall: "0",
-  },
-  {
-    employeeId: "02",
-    name: "Itu",
-    sayi1: "0",
-    sayi2: "0",
-    sayi3: "0",
-    sayi4: "0",
-    sayi5: "0",
-    overall: "0",
-  },
-  {
-    employeeId: "03",
-    name: "BOUN",
-    sayi1: "0",
-    sayi2: "0",
-    sayi3: "0",
-    sayi4: "0",
-    sayi5: "0",
-    overall: "0",
+    overall: "",
   },
 ];
 
 const EditableTable = () => {
   const [employeeData, setEmployeeData] = useState(data);
-  const [katsayi1, setKatSayi1] = useState(6);
-  const [katsayi2, setKatSayi2] = useState(6);
-  const [katsayi3, setKatSayi3] = useState(6);
-  const [katsayi4, setKatSayi4] = useState(6);
-  const [katsayi5, setKatSayi5] = useState(6);
+  const [katsayi1, setKatSayi1] = useState(0);
+  const [katsayi2, setKatSayi2] = useState(0);
+  const [katsayi3, setKatSayi3] = useState(0);
+  const [katsayi4, setKatSayi4] = useState(0);
+  const [katsayi5, setKatSayi5] = useState(0);
 
   const AddNumbers = (sayi1, sayi2, sayi3, sayi4, sayi5, employeeId) => {
     var totImp = parseFloat(katsayi1) + parseInt(katsayi2) + parseInt(katsayi3) + parseInt(katsayi4) + parseInt(katsayi5);
@@ -157,6 +137,8 @@ const EditableTable = () => {
 
   const onSortButtonClick = () => { const ovrAscending = [...employeeData].sort((a, b) => b.overall < a.overall ? -1 : 1); setEmployeeData(ovrAscending); };
 
+
+
   return (
     <div class="h-screen font-sans  bg-gray-700">
       <div class="w-full p-4 border border-gray-200 bg-gray-50 rounded-t-xl dark:border-gray-600 dark:bg-gray-800">
@@ -201,6 +183,7 @@ const EditableTable = () => {
                   name="name"
                   value={name}
                   type="text"
+                  min="0" max="10"
                   onChange={(e) => onChangeInput(e, employeeId)}
                   placeholder="Type Name"
                 />
@@ -209,6 +192,7 @@ const EditableTable = () => {
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="katsayi"
                   type="number"
+                  min="0" max="10"
                   value={katsayi1}
                   onChange={(e) => onKatsayi1Change(e, employeeId, katsayi1)}
                   placeholder="Type Importance"
@@ -219,6 +203,7 @@ const EditableTable = () => {
                   name="sayi1"
                   value={sayi1}
                   type="number"
+                  min="0" max="10"
                   onChange={(e) => onChangeInput(e, employeeId)}
                   placeholder="Type Points"
                 />
@@ -227,6 +212,7 @@ const EditableTable = () => {
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="katsayi"
                   type="number"
+                  min="0" max="10"
                   value={katsayi2}
                   onChange={(e) => onKatsayi2Change(e, employeeId, katsayi1)}
                   placeholder="Type Importance"
@@ -235,7 +221,8 @@ const EditableTable = () => {
               <td class="px-2 py-4">
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="sayi2"
-                  type="text"
+                  type="number"
+                  min="0" max="10"
                   value={sayi2}
                   onChange={(e) => onChangeInput(e, employeeId)}
                   placeholder="Type Points"
@@ -247,6 +234,7 @@ const EditableTable = () => {
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="katsayi"
                   type="number"
+                  min="0" max="10"
                   value={katsayi3}
                   onChange={(e) => onKatsayi3Change(e, employeeId, katsayi1)}
                   placeholder="Type Importance"
@@ -255,7 +243,8 @@ const EditableTable = () => {
               <td class="px-2 py-4">
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="sayi3"
-                  type="text"
+                  type="number"
+                  min="0" max="10"
                   value={sayi3}
                   onChange={(e) => onChangeInput(e, employeeId)}
                   placeholder="Type Points"
@@ -265,15 +254,18 @@ const EditableTable = () => {
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="katsayi"
                   type="number"
+                  min="0" max="10"
                   value={katsayi4}
                   onChange={(e) => onKatsayi4Change(e, employeeId, katsayi1)}
                   placeholder="Type Importance"
+                  pattern="[0-10]"
                 />
               </td>
               <td class="px-2 py-4">
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="sayi4"
-                  type="text"
+                  type="number"
+                  min="0" max="10"
                   value={sayi4}
                   onChange={(e) => onChangeInput(e, employeeId)}
                   placeholder="Type Points"
@@ -283,6 +275,7 @@ const EditableTable = () => {
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="katsayi"
                   type="number"
+                  min="0" max="10"
                   value={katsayi5}
                   onChange={(e) => onKatsayi5Change(e, employeeId, katsayi1)}
                   placeholder="Type Importance"
@@ -291,7 +284,8 @@ const EditableTable = () => {
               <td class="px-2 py-4">
                 <input class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="grid-last-name"
                   name="sayi5"
-                  type="text"
+                  type="number"
+                  min="0" max="10"
                   value={sayi5}
                   onChange={(e) => onChangeInput(e, employeeId)}
                   placeholder="Type Points"
@@ -307,14 +301,15 @@ const EditableTable = () => {
                     value={AddNumbers(sayi1, sayi2, sayi3, sayi4, sayi5, employeeId)}
                     onChange={(e) => onChangeInput(e, employeeId)}
                     placeholder="Type Position"
-                  /></div>
-                  
-                  <div class="authButtons basis-1/4  flex flex-col items-center justify-center">   <button class="" onClick={(e) => onDeleteButtonClick(e, employeeId)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-center dark:text-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                      </svg>
 
-                    </button> </div>
+                  /></div>
+
+                  <div class="authButtons basis-1/4  flex flex-col items-center justify-center">   <button class="" onClick={(e) => onDeleteButtonClick(e, employeeId)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-center dark:text-white">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+
+                  </button> </div>
                 </div>
               </td>
             </tr>
@@ -335,8 +330,8 @@ const EditableTable = () => {
       </div>
       <div class="p-6 text-gray-900 whitespace-nowrap dark:text-white font-normal text-lg">
         <p>1.Enter the university names</p>
-        <p>2.Enter the importances for all criteria (1-10)</p>
-        <p>3.Enter points for each criteria of each school (1-10)</p>
+        <p>2.Enter the importances for all criteria (0-10)</p>
+        <p>3.Enter points for each criteria of each school (0-10)</p>
         <p>4.Press + if you want to add school</p>
         <p>5.Press sorting button to sort according to the overall score</p>
       </div>
